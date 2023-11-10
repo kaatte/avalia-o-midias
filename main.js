5c2bbe502d1df04975ab6d30d3867509e5d144bd
kety <106088213+kaatte@users.noreply.github.com>
2023-11-10 10:48:12 -0300

    km

---
+++ main.js
@@ -0,0 +1,15 @@
+function tocasom (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+while(contador < lista listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador]classlist(1)
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclik = function(){
+        tocasom (idAudio)
+    }
+    contador = contador +1;
+    console.log(contador);
+}
