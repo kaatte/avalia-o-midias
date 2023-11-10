dd2c40d421666bff44abc4bad0ad1f60154d0cd0
kety <106088213+kaatte@users.noreply.github.com>
2023-11-10 10:51:11 -0300

    ,

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let contador = 0;
 while(contador < listaDeTeclas.length){
     const efeito = listaDeTeclas[contador].classlist(1);
     const idAudio = "#som_"+efeito;
-    listaDeTeclas[contador].onclik = function(){
+    listaDeTeclas[contador].onclick = function(){
         tocasom (idAudio)
     }
     contador = contador +1;
