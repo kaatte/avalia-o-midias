bbfedef76d765cfe1375b2928e2f481146bfece6
kety <106088213+kaatte@users.noreply.github.com>
2023-11-10 10:52:19 -0300

    d

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
 while(contador < listaDeTeclas.length){
-    const efeito = listaDeTeclas[contador].classlist(1);
+    const efeito = listaDeTeclas[contador].classList(1);
     const idAudio = "#som_"+efeito;
     listaDeTeclas[contador].onclick = function(){
         tocasom (idAudio)
